f3fc84748f6b824a6569c398512848d58e08ad3f
Ran Chen <chenr09@gmail.com>
2025-03-05 17:17:51 -0800

    remove something temproraly

--- app/(chat)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Chat } from "@/components/chat";
-import { generateId } from "ai";
-import { auth } from "@/app/(auth)/auth";
-
-export default async function Page() {
-  const session = await auth();
-  return <Chat id={generateId()} initialMessages={[]} session={session} />;
-}
